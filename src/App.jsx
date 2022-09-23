c84735804b831dc286ab85bfaaac13ed191dad92
Obaid Ur Rehman <obaidurrehman8121@gmail.com>
2022-09-24 02:06:11 +0500

    slot game

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import react from "react";
+import SlotMach from "./SlotMach";
+
+
+const App = () => {
+    return <>
+    <h1 className="heading_style"> 🎰 Welcome To <strong>Slot Machine Game</strong>🎰 </h1>
+    
+    <div>
+        <SlotMach x="❤" y="❤" z="❤"></SlotMach>
+        <hr/>
+        <SlotMach x="🖤" y="❤" z="😃"></SlotMach>
+        <hr/>
+        <SlotMach x="💑" y="😎" z="👩‍❤️‍👩"></SlotMach>
+        <hr/>
+        <SlotMach x="😣" y="😣" z="😣"></SlotMach>
+
+        
+    </div>
+    </>
+}
+
+
+export default App;
